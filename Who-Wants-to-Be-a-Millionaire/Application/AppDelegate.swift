9058bbc3cc2da97c756a2d987f7703b0415a11f2
Aleksei <aleksei@mail.ru>
2023-02-06 20:49:52 +0300

    Initial commit

---
+++ Who-Wants-to-Be-a-Millionaire/Application/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by Алексей on 06.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
