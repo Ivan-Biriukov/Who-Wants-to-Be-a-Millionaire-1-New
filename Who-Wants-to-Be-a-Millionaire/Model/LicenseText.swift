5eb2d1aa9e87d9f39c94b932dfac748e51412a45
Ivan Biriukov <Grottt0@mail.ru>
2023-02-07 20:44:21 +0300

    RulesViewController - Done!
    Finish RulesViewController + add Fonts

---
+++ Who-Wants-to-Be-a-Millionaire/Model/LicenseText.swift
@@ -0,0 +1,8 @@
+//
+//  LicenseText.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by иван Бирюков on 07.02.2023.
+//
+
+import Foundation
---
+++ Who-Wants-to-Be-a-Millionaire/RulesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RulesViewController.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by иван Бирюков on 07.02.2023.
+//
+
+import Foundation
