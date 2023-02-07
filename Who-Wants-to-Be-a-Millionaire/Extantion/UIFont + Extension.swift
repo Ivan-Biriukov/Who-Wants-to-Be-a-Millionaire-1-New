96b7644d9ca85778218a646f863162b06828b17f
Aleksei <aleksei@mail.ru>
2023-02-07 08:38:39 +0300

    add UIColor + Extension, UIFont + Extension

---
+++ Who-Wants-to-Be-a-Millionaire/Extantion/UIFont + Extension.swift
@@ -0,0 +1,38 @@
+//
+//  UIFont + Extensions.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by иван Бирюков on 05.02.2023.
+//
+
+import UIKit
+
+extension UIFont{
+    
+    // Medium
+    static func robotoMedium24() -> UIFont?{
+        return UIFont.init(name: "Roboto-Medium", size: 24)
+    }
+    
+    static func robotoMedium12() -> UIFont?{
+        return UIFont.init(name: "Roboto-Medium", size: 12)
+    }
+    
+    static func robotoMedium14() -> UIFont?{
+        return UIFont.init(name: "Roboto-Medium", size: 14)
+    }
+    
+    static func robotoMedium18() -> UIFont?{
+        return UIFont.init(name: "Roboto-Medium", size: 18)
+    }
+    
+    //bold
+    static func robotoBold16() -> UIFont?{
+        return UIFont.init(name: "Roboto-Bold", size: 16)
+    }
+    
+    static func robotoBold20() -> UIFont?{
+        return UIFont.init(name: "Roboto-Bold", size: 20)
+    }
+    
+}
